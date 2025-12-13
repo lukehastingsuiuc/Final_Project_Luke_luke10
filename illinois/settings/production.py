82582d809cb2bdaeca44686295ac1dbfa27c9244
Luke Hastings <luke10@illinois.edu>
2025-12-12 20:43:02 -0600

    altered database for pythonanywhere

--- illinois/settings/production.py
+++ illinois/settings/production.py
@@ -1,3 +1,13 @@
 from .base import *
 DEBUG = False
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'luke10.pythonanywhere.com']
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "luke10$info390mg-mysql-database",
+        "USER": "mohitg2",
+        "PASSWORD": "graingerlibrary",
+        "HOST": "luke10.mysql.pythonanywhere-services.com",
+        "PORT": "3306",
+    }
+}
